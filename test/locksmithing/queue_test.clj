e53388c11362313b43353ee3674136aefdd5cb97
Max Countryman <max.countryman@gmail.com>
2014-05-01 06:53:16 -0700

    queue test enlarged

--- test/locksmithing/queue_test.clj
+++ test/locksmithing/queue_test.clj
@@ -1,13 +1,11 @@
 (ns locksmithing.queue-test
-  (:refer-clojure :exclude [pop])
+  (:refer-clojure :exclude [pop!])
   (:require [clojure.test :refer [deftest is]]
-            [locksmithing.queue :refer [queue pop push]]))
+            [locksmithing.queue :refer [queue pop! push!]]))
 
 (deftest concurrent-enqueues
   (let [q (queue)]
-    (->> (range 1e4)
-         (pmap (partial push q))
-          dorun)
-    (let [elements (->> (repeatedly #(pop q))
-                        (take-while (complement nil?)))]
-      (is (= (sort elements) (range 1e4))))))
+    (->> (range 10000) (pmap #(push! q %)) dorun)
+    (let [values (->> (repeatedly #(pop! q))
+                      (take-while (complement nil?)))]
+      (is (= (sort values) (range 10000))))))
