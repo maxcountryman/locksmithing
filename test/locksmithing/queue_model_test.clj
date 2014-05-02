116a34683e88a5c0f2ce36caf81330219072c38c
Max Countryman <max.countryman@gmail.com>
2014-05-01 18:03:06 -0700

    model tests updated for queue

--- test/locksmithing/queue_model_test.clj
+++ test/locksmithing/queue_model_test.clj
@@ -1,9 +1,9 @@
 (ns locksmithing.queue-model-test
-  (:require [clojure.pprint :refer [pprint]]
-            [clojure.test :refer [deftest is]]
+  (:require [clojure.test :refer [deftest is]]
             [knossos.core :refer [inconsistent linearizations op]]
+            [knossos.history :refer [complete]]
             [knossos.util :refer [update]]
-            [locksmithing.queue :refer [queue]])
+            [locksmithing.queue :as q])
   (:import [java.util.concurrent.atomic AtomicReference]
            [knossos.core Model]
            [locksmithing.queue Node]))
@@ -35,7 +35,7 @@
   "Our system contains an instance of locksmithing.queue.Queue and a history
   vector, formatted as a map."
   []
-  {:queue   (queue)
+  {:queue   (q/queue)
    :counter (atom 0)  ;; push value counter
    :history []})
 
@@ -43,139 +43,130 @@
 (declare queue-push-start)
 (declare queue-push-2)
 (declare queue-push-3)
-(declare queue-push-4)
 (declare queue-push-complete)
-(declare maybe-push-complete)
 
 (defn push-state
+  "Given an id, returns the id appended to the string 'push-state-' and cast
+  as a keyword."
   [id]
   (keyword (str "push-state-" (name id))))
 
 (defn push-transition
+  "Given an id, returns the id appended to the string 'push-transition-' and
+  cast as a keyword."
   [id]
   (keyword (str "push-transition-" (name id))))
 
 (defn queue-push-start
-  "Given a system and optionally a value to push (otherwise grabs one from
-  push-state), sets up the initial phase of the push method. This involves
-  acquiring a head node and setting up a candidate replacement head containing
-  the value to push and the previous head, as a tail value. These values are
-  associated into the system as push-state.
-
-  Sets queue-push-2 as the transition function. Updates the history vector of
-  the system.
-
-  Returns the system map."
-  [sys id & [v]]
-  (let [v    (or v (swap! (:counter sys) inc))
-        head (-> sys :queue .head-ref .get)]
+  "Given a system map sys, an id, and optionally a Node n, sets up the initial
+  phase of a push operation.
+  
+  This models locksmithing.queue/push! by setting up the state that will be
+  used throughout the state transitions. Once set up, the state is contained
+  in a map and added to the system map.
+  
+  If a node is provided, this call is treated like a recur invocation and the
+  node currently contained in the state map is used again. However, the
+  remaining state is rebound.
+  
+  Always sets the state transition to queue-push-2.
+  
+  Returns a system map."
+  [sys id & [n]]
+  (let [loop? (not (nil? n))
+        n     (or n (q/node (swap! (:counter sys) inc) nil))
+        t     (-> sys :queue q/get-tail)
+        t'    @t
+        s     @(q/get-next @t)]
     (-> sys
-        (assoc (push-state id)
-               {:v     v
-                :head  head
-                :head' (Node. v (AtomicReference. nil) head)})
+        (assoc (push-state id) {:n n :t t :t' t' :s s})
         (assoc (push-transition id) queue-push-2)
-        (update (op (push-state id) :invoke :push v)))))
+        (update (op (push-state id)
+                    (if loop? :info :invoke)
+                    :push
+                    (q/get-value n))))))
 
 (defn queue-push-2
-  "Given a system, models the second phase of the push method. This involves
-  branching on the value of head (as defined in queue-push-start). When falsey
-  (i.e. the first value pushed onto the queue) a CAS operation takes place over
-  the head reference currently in the system and the head acquired in
-  push-queue-start. If this operation fails, the transition is set to
-  queue-push-start. Otherwise it succeeds and transition is set to
-  queue-push-complete. When a truthy head is found a CAS operation takes place
-  over the value of the prev-ref field of the head. Much like above if this
-  fails transition is set to queue-push-start. Otherwise it succeeds and
-  transition is set to queue-push-3.
-
-  Sets the transition function to either queue-push-start, queue-push-3, or
+  "Given a system map sys and an id, models a linearizability point of
+  locksmithing.queue/push!.
+  
+  If the bound value of tail, in our saved state, is consistent with the value
+  in the actual queue, we advance. When not, the transition will be
+  queue-push-start, i.e. we recur.
+  
+  When successor node is nil, an invocation of cas-next is made. A truthy
+  value indicates a successful push and the transition is set to
+  queue-push-complete. Otherwise we set the transition to queue-push-3.
+  
+  Sets the state transition to queue-push-start, queue-push-3, or
   queue-push-complete.
-
-  Returns the system map."
+  
+  Returns a system map."
   [sys id]
-  (let [state-key   (push-state id)
-        v           (-> sys state-key :v)
-        head        (-> sys state-key :head)
-        head'       (-> sys state-key :head')
-        transition  (atom #(queue-push-start %1 %2 v))]
+  (let [state-key  (push-state id)
+        n          (-> sys state-key :n)
+        t          (-> sys state-key :t)
+        t'         (-> sys state-key :t')
+        s          (-> sys state-key :s)
+        transition (atom #(queue-push-start %1 %2 n))]
 
-    (if head
-      ;; 1a. in locksmithing.queue
-      (do (when (.compareAndSet (.prev-ref head) nil head')
-            (reset! transition queue-push-3))
+    (when (identical? t' (-> sys :queue q/get-tail deref))
+      (if (nil? s)
+        (when (q/cas-next t s n)
+          (reset! transition queue-push-complete))
+        (reset! transition queue-push-3)))
 
-          (-> sys
-              (assoc (push-transition id) @transition)
-              (update (op state-key :info :queue-push-2 v))))
-
-      ;; 1b. in locksmithing.queue
-      (do (when (.compareAndSet (-> sys :queue .head-ref) head head')
-            (reset! transition queue-push-complete))
-          (.set (-> sys :queue .tail-ref) head')
-
-          (-> sys
-              (assoc (push-transition id) @transition)
-              (update (op state-key :info :queue-push-2 v))
-              (maybe-push-complete id))))))
+    (-> sys
+        (assoc (push-transition id) @transition)
+        (update (op state-key :info :queue-push-2 (q/get-value n))))))
 
 (defn queue-push-3
-  "Given a system, models the third (optional) phase of the push method. This
-  involves completing the truthy branch of queue-push-2. A CAS operation takes
-  place over the head reference currently in the system and the head acquired
-  in queue-push-start. If this operation fails, the transition is set to
-  queue-push-start. Otherwise it succeeds and transition is set to
-  queue-push-complete.
-
-  Sets the transition function to either queue-push-start or
-  queue-push-complete.
-
-  Returns the system map."
+  "Given a system map sys and an id, models a linearizability point of
+  locksmithing.queue/push!.
+  
+  This is the resulting branch of a cas-next failure. Here cas-tail is invoked
+  using the stored state, over t' and s.
+  
+  The transition is always set to queue-push-start.
+  
+  Returns a system map."
   [sys id]
   (let [state-key   (push-state id)
-        v           (-> sys state-key :v)
-        head        (-> sys state-key :head)
-        head'       (-> sys state-key :head')
-        transition  (atom #(queue-push-start %1 %2 v))]
+        n           (-> sys state-key :n)
+        t'          (-> sys state-key :t')
+        s           (-> sys state-key :s)
+        transition  (atom #(queue-push-start %1 %2 n))]
 
-    ;; 2. in locksmithing.queue
-    (do (when (.compareAndSet (-> sys :queue .head-ref) head head')
-          (reset! transition queue-push-complete))
+    (q/cas-tail (:queue sys) t' s)
 
-        (-> sys
-            (assoc (push-transition id) @transition)
-            (update (op state-key :info :queue-push-3 v))
-            (maybe-push-complete id)))))
-
-(defn maybe-push-complete
-  "Given a system, conditionally sets the op completion in the system history
-  if the current transition function is queue-push-complete. Calling this
-  directly after any potentially completing actions ensures that there is no
-  gap between a valid completion and further system states.
-
-  Returns the system map."
-  [sys id]
-  (let [transition (get sys (push-transition id))]
-    (if (identical? transition queue-push-complete)
-      (queue-push-complete sys id)
-      sys)))
+    (update sys (op state-key :info :queue-push-3 (q/get-value n)))))
 
 (defn queue-push-complete
-  "Given a system, models a successful push.
-
-  Returns the system map."
+  "Given a system map sys and an id, completes a push with the current stored
+  state. Models a linearizability point of locksmithing.queue/push!.
+  
+  Before resetting the push-transition to nil, we invoke cas-tail over the
+  queue in the system, t', and n, in an attempt to advance the tail.
+  
+  The transition is always set to nil.
+  
+  Returns a system map."
   [sys id]
   (let [state-key (push-state id)
-        v         (-> sys state-key :v)]
+        n         (-> sys state-key :n)
+        t'        (-> sys state-key :t')]
+
+    (q/cas-tail (:queue sys) t' n)
+
     (-> sys
-      (dissoc (push-transition id))  ;; Reset the push-transition fn
-      (update (op state-key :ok :push v)))))
+        (dissoc (push-transition id))  ;; Reset the push-transition fn
+        (update (op state-key :ok :push (q/get-value n))))))
 
 (defn queue-push
-  "Given a system, nodels the push lock-free algorithm via a set of state
-  transitions.
-
-  Returns the system map."
+  "Given a system map sys and an id, handles state transitions in the push
+  operation state machine.
+  
+  Returns a system map."
   [sys id]
   (let [transition  (get sys (push-transition id))
         transition' (or transition queue-push-start)]
@@ -186,138 +177,151 @@
 (declare queue-pop-2)
 (declare queue-pop-3)
 (declare queue-pop-complete)
-(declare maybe-pop-complete)
 
 (defn pop-state
+  "Given an id, returns the id appended to the string 'pop-state-' and cast
+  as a keyword."
   [id]
   (keyword (str "pop-state-" (name id))))
 
 (defn pop-transition
+  "Given an id, returns the id appended to the string 'pop-transition-' and
+  cast as a keyword."
   [id]
   (keyword (str "pop-transition-" (name id))))
 
 (defn queue-pop-start
-  "Given a system sets up the initial phase of the pop method. This involves
-  first ensuring and acquiring a tail. Note that if no tail exists, there is
-  nothing to pop and the system map is simply returned immediately. Otherwise
-  the previous tail, head, and value are retrieved and associated into the
-  system as pop-state.
+  "Given a system map sys, an id, and optionally a boolean loop?, sets up the
+  initial phase of a pop operation.
+  
+  This models locksmithing.queue/pop! by setting up the state that will be
+  used throughout the state transitions. Once set up, the state is contained
+  in a map and added to the system map.
+  
+  If loop? is truthy, this call is treated like a recur invocation. State is
+  once again rebound.
+  
+  Always sets the state transition to queue-pop-2.
+  
+  Returns a system map."
+  [sys id & [loop?]]
+  (let [h  (-> sys :queue q/get-head)
+        h' @h
+        t  @(-> sys :queue q/get-tail)
+        f  (q/get-next @h)
+        f' @f]
 
-  Sets queue-pop-2 as the transition function.
-
-  Returns the system map."
-  [sys id]
-  (if-let [tail (-> sys :queue .tail-ref .get)]  ;; Cannot pop without tail
-    (let [tail' (-> tail .prev-ref .get)
-          head  (-> sys :queue .head-ref .get)
-          v     (.data tail)]
-      (-> sys
-          (assoc (pop-state id) {:tail  tail
-                                 :tail' tail'
-                                 :head  head
-                                 :v     v})
-          (assoc (pop-transition id) queue-pop-2)
-          (update (op (pop-state id) :invoke :pop v))))
-    sys))
+    (-> sys
+        (assoc (pop-state id) {:h h :h' h' :t t :f f :f' f'})
+        (assoc (pop-transition id) queue-pop-2)
+        (update (op (pop-state id)
+                    (if loop? :info :invoke)
+                    :pop
+                    nil)))))
 
 (defn queue-pop-2
-  "Given a system, models the second phase of the pop method. This involves
-  checking if the tail and head acquired in queue-pop-start are the same. When
-  truthy a CAS takes place over the value of the head-ref currently in the
-  system and head. If this succeeds the transition function is set to
-  queue-pop-3. Otherwise queue-pop-start.
-
-  Sets either queue-pop-start or queue-pop-3 as the transition function.
-
-  Returns the system map."
+  "Given a sytem map sys and a id, models a linearizability point of
+  locksmithing.queue/pop!.
+  
+  If the bound value of head, in our saved state, is consistent with the value
+  in the actual queue, we advance. When not, the transition will be
+  queue-pop-start, i.e. we recur.
+  
+  We then ask if the bound head and tail are consistently. If truthy we ask if
+  the saved first node is nil. If it is, the queue must be empty and we set
+  the transition to queue-pop-complete. If it's not, then we cas-tail over the
+  queue, the bound tail and first node, i.e. we attempt to advance the lagging
+  tail.
+  
+  When the head and tail are inconsistent, we ask if cas-head over the queue,
+  the bound head and first node is true. This means we have a value to pop,
+  assuming the first node contains non-nil. As long as this is true, we set
+  the transition to queue-pop-3.
+  
+  Sets the transiton to queue-pop-start, queue-pop-3, or queue-pop-complete.
+  
+  Returns a system map."
   [sys id]
   (let [state-key  (pop-state id)
-        v          (-> sys state-key :v)
-        head       (-> sys state-key :head)
-        tail       (-> sys state-key :tail)
-        transition (atom queue-pop-3)]
+        h'         (-> sys state-key :h')
+        t          (-> sys state-key :t)
+        f'         (-> sys state-key :f')
+        v          (atom nil)
+        transition (atom #(queue-pop-start %1 %2 true))]
 
-    ;; 1. in locksmithing.queue
-    (when (identical? tail head)
-      (when-not (.compareAndSet (-> sys :queue .head-ref) head nil)
-        (reset! transition queue-pop-start)))
+    (when (identical? h' @(q/get-head (:queue sys)))
+      (if (identical? h' t)
+        (if (nil? f')
+          (reset! transition queue-pop-complete)
+          (q/cas-tail (:queue sys) t f'))
+        (when (q/cas-head (:queue sys) h' f')
+          (when-let [v' (q/get-value f')]
+            (reset! v v')
+            (reset! transition queue-pop-3)))))
 
     (-> sys
         (assoc (pop-transition id) @transition)
-        (update (op state-key :info :queue-pop-2 v)))))
+        (assoc-in [state-key :v] @v)
+        (update (op state-key :info :queue-pop-2 @v)))))
 
 (defn queue-pop-3
-  "Given a system, models the third phase of the pop method. This involves a
-  CAS over the value of the tail-ref currently in the system and the tail
-  acquired in queue-pop-start. If this succeeds, the transition function is set
-  to queue-pop-complete. Otherwise queue-pop-start.
-
-  Sets either queue-pop-start or queue-pop-complete as the transition function.
-
-  Returns the system map."
+  "Given a sytem map sys and a id, models a linearizability point of
+  locksmithing.queue/pop!.
+  
+  We unconditionally set the value of the first node to nil, effectively
+  deleting it.
+  
+  Always sets the transition to queue-pop-complete.
+  
+  Returns a system map."
   [sys id]
   (let [state-key  (pop-state id)
+        f          (-> sys state-key :f)
         v          (-> sys state-key :v)
-        tail       (-> sys state-key :tail)
-        tail'      (-> sys state-key :tail')
-        transition (atom queue-pop-start)]
+        transition (atom queue-pop-complete)]
 
-    ;; 2. in locksmithing.queue
-    (when (.compareAndSet (-> sys :queue .tail-ref) tail tail')
-      (reset! transition queue-pop-complete))
+    (q/set-value f nil)
 
     (-> sys
         (assoc (pop-transition id) @transition)
-        (update (op state-key :info :queue-pop-3 v))
-        (maybe-pop-complete id))))
-
-(defn maybe-pop-complete
-  "Given a system, conditionally sets the op completion in the system history
-  if the current transition function is queue-pop-complete. Calling this
-  directly after any potentially completing actions ensures that there is no
-  gap between a valid completion and further system states.
-
-  Returns the system map."
-  [sys id]
-  (let [transition (get sys (pop-transition id))]
-    (if (identical? transition queue-pop-complete)
-      (queue-pop-complete sys id)
-      sys)))
+        (update (op state-key :info :queue-pop-3 v)))))
 
 (defn queue-pop-complete
-  "Given a system, models a successful push.
-
-  Returns the system map."
+  "Given a system map sys and an id, completes a pop with the current stored
+  state.
+  
+  The transition is always set to nil.
+  
+  Returns a system map."
   [sys id]
   (let [state-key (pop-state id)
         v         (-> sys state-key :v)]
     (-> sys
-      (dissoc (pop-transition id))  ;; Reset the pop-transition fn
-      (update (op state-key :ok :pop v)))))
+        (dissoc (pop-transition id))  ;; Reset the pop-transition fn
+        (update (op state-key :ok :pop v)))))
 
 (defn queue-pop
-  "Given a system, nodels the pop lock-free algorithm via a set of state
-  transitions.
-
-  Returns the system map."
+  "Given a system map sys and an id, handles state transitions in the pop
+  operation state machine.
+  
+  Returns a system map."
   [sys id]
   (let [transition  (get sys (pop-transition id))
         transition' (or transition queue-pop-start)]
     (transition' sys id)))
 
 ;; Operational models, e.g. concurrent push operations
-(defn concurrent-push-step
+(defn concurrent-push-steps
   "System transitions related to applying concurrent push operations."
   [sys]
   (list #(queue-push sys :a)
         #(queue-push sys :b)
         #(queue-pop  sys :a)))
 
-(defn concurrent-pop-step
-  "System transitions related to applying concurrent pop operations."
+(defn concurrent-push-pop-steps
+  "System transitions related to applying concurrent push/pop operations."
   [sys]
   (list #(queue-push sys :a)
-        #(queue-pop  sys :a)
         #(queue-pop  sys :b)))
 
 (defn trajectory
@@ -331,20 +335,20 @@
 ;; Tests
 (deftest concurrent-push-test
   (dothreads [_ 4]
-    (dotimes [_ 1e4]
-      (let [system  (trajectory (system) concurrent-push-step 30)
-            history (:history system)
+    (dotimes [_ 100]
+      (let [system  (trajectory (system) concurrent-push-steps 50)
+            history (-> system :history complete)
             model   (QueueModel. nil)
             linears (linearizations model history)]
 
         ;; Is this history linearizable?
         (is (not (empty? linears)))))))
 
-(deftest concurrent-pop-test
+(deftest concurrent-push-pop-test
   (dothreads [_ 4]
-    (dotimes [_ 1e3]
-      (let [system  (trajectory (system) concurrent-pop-step 30)
-            history (:history system)
+    (dotimes [_ 100]
+      (let [system  (trajectory (system) concurrent-push-pop-steps 50)
+            history (-> system :history complete)
             model   (QueueModel. nil)
             linears (linearizations model history)]
 
